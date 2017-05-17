9f1b081406427921a774c18bd9b03048959fbf90
sergioviniciuss <sergioviniciuss@gmail.com>
2017-05-17 00:30:44 -0300

    Starting project from boilerplate

---
+++ react-bp-apps/Project-1/hello-world/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return(
+      <div>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+      )
+  }
+}
+
+class Intro extends Component {
+  render() {
+    return(
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+      )
+  }
+}
+
+export default App;
