9549c559833ecded38d32b31fd845d0c091c9479
sergioviniciuss <sergioviniciuss@gmail.com>
2017-05-18 00:11:53 -0300

    creating components for this basic hello world sample

--- react-bp-apps/Project-1/hello-world/src/App.js
+++ react-bp-apps/Project-1/hello-world/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Intro from './Intro';
 
 class App extends Component {
   render() {
@@ -10,31 +12,6 @@ class App extends Component {
         <Intro />
       </div>
     );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return(
-      <div>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
-      )
-  }
-}
-
-class Intro extends Component {
-  render() {
-    return(
-      <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-      )
   }
 }
 
