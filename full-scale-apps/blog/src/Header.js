dd6dbeb3204163f23e97adc57fc438d6184c8399
sergioviniciuss <sergioviniciuss@gmail.com>
2017-06-01 23:10:04 -0300

    Removing useless class on clean-blog and fixing error in Header

--- full-scale-apps/blog/src/Header.js
+++ full-scale-apps/blog/src/Header.js
@@ -4,18 +4,18 @@ export default class Header extends Component {
 	render() {
 		return(
 			<header className="intro-header" style={{"background-image" : "url('img/home-bg.jpg')"}}>
-              <div className="container">
-                  <div className="row">
-                      <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                          <div className="site-heading">
-                              <h1>Clean Blog</h1>
-                              <hr className="small" />
-                              <span className="subheading">A Clean Blog Theme by Start Bootstrap</span>
-                          </div>
+          <div className="container">
+              <div className="row">
+                  <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                      <div className="site-heading">
+                          <h1>Clean Blog</h1>
+                          <hr className="small" />
+                          <span className="subheading">A Clean Blog Theme by Start Bootstrap</span>
                       </div>
                   </div>
               </div>
-          </header>
+          </div>
+      </header>
 		)
 	}
 }
