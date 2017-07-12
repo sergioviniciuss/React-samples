c5d94ff5f2735f6bb0114ac8c0def91a388adb6e
sergioviniciuss <sergioviniciuss@gmail.com>
2017-07-11 22:14:30 -0300

    Simple Login authentication using Auth0 and react-bootstrap

---
+++ full-scale-apps/auth0-github-api/src/Github.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Github extends Component {
+	render() {
+		return(
+			<div>
+				I am comming from github
+			</div>
+		)
+	}
+}
+
+export default Github;
