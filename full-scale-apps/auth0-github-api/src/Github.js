e393e115cc834d2090b04de616be5ac435491bc6
sergioviniciuss <sergioviniciuss@gmail.com>
2017-07-13 23:54:03 -0300

    Fetching github's api

--- full-scale-apps/auth0-github-api/src/Github.js
+++ full-scale-apps/auth0-github-api/src/Github.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import Profile from './Components/Profile';
+import Search from './Components/Search';
+
+const API = 'https://api.github.com/users';
+
+
 
 class Github extends Component {
+
+	constructor(props){
+		super(props);
+	
+		this.state = {
+			username: 'sergioviniciuss',
+			name: '',
+			avatar: '',
+			repos: '',
+			followers: '',
+			following: '',
+			homeURL: '',
+			notFound: ''
+		};
+	}
+
+	getProfile(username) {
+		let finalURL = `${API}/${username}`;
+
+		fetch(finalURL)
+		.then((res) => res.json())
+		.then((data) => {
+			this.setState({
+				username: data.login,
+				name: data.name,
+				avatar: data.avatar_url,
+				repos: data.public_repos,
+				followers: data.followers,
+				following: data.following,
+				homeURL: data.html_url,
+				notFound: data.message
+			})
+		})
+		.catch((error)=> console.log("there was a problem in fetching data"))
+	}
+	componentDidMount() {
+		this.getProfile(this.state.username);
+	}
 	render() {
 		return(
 			<div>
-				I am comming from github
+				<section id="card">
+					<Search />
+					<Profile />
+				</section>
 			</div>
 		)
 	}
