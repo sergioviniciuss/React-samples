113f5c1cc0869e213f43bdf14c3c7fdb63d8fb98
sergioviniciuss <sergioviniciuss@gmail.com>
2017-07-14 01:17:45 -0300

    Github profile searcher

--- full-scale-apps/auth0-github-api/src/Components/Search.js
+++ full-scale-apps/auth0-github-api/src/Components/Search.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+
 class Search extends Component {
+
+	submit(event) {
+		event.preventDefault();
+		let value = this.refs.username.value;
+		this.props.searchProfile(value);
+		this.refs.username.value = '';
+	}
 	render() {
 		return(
-			<div>
-				I am comming from Search
+			<div className="search-box">
+				<form onSubmit={this.submit.bind(this)}>
+					<label><input type="search" ref="username" placeholder="Type a username"/></label>
+				</form>
 			</div>
 		)
 	}
